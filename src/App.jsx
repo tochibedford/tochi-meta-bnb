77201809110dc7c067c56c95ec10c3c69b562f28
tochibedford <musicofbyte@gmail.com>
2022-11-11 13:58:41 +0100

    feat: implemented responsive navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import styles from './App.module.scss'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <div className="App">
+      <Navbar />
+      <main className={styles.main__content}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam eaque quo libero placeat repellat, recusandae totam cum dicta dolorum consectetur, repudiandae earum perspiciatis, magnam eum facere nisi inventore nesciunt numquam?
+      </main>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,68 @@
+import styles from '../styles/Navbar.module.scss'
+import MetaBrand from '../assets/MetaBrand.svg'
+
 const Navbar = () => {
+    const menuItems = [
+        {
+            title: 'Home',
+            url: '/'
+        },
+        {
+            title: 'Place to Stay',
+            url: '/placetostay'
+        },
+        {
+            title: 'NFTs',
+            url: '/nfts'
+        },
+        {
+            title: 'Community',
+            url: '/community'
+        },
+
+    ]
     return (
-        <nav>
-            {/* <div className=></div> */}
+        <nav className={styles.navbar}>
+            <div className={styles.brand__container}>
+                <img src={MetaBrand} className={styles.brand} alt="brand logo" />
+            </div>
+            <div className={styles.scalable__nav}>
+                <ul className={styles.main__nav}>
+                    {menuItems.map((item, index) => {
+                        return (
+                            <li key={item.url + index} className={styles.main__nav__item}>
+                                <a href={item.url}>{item.title}</a>
+                            </li>
+                        )
+                    })}
+                </ul>
+                <div className={styles.wallet__button__container}>
+                    <button className={styles.wallet__button}>Connect Wallet</button>
+                </div>
+            </div>
+            <div className={styles.mobile__menu}>
+                
+                <div className={styles.burger__menu}>
+                    <input type="checkbox"/>
+                    <div className={styles.scalable__nav}>
+                        <ul className={styles.main__nav}>
+                            {menuItems.map((item, index) => {
+                                return (
+                                    <li key={item.url + index} className={styles.main__nav__item}>
+                                        <a href={item.url}>{item.title}</a>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                        <div className={styles.wallet__button__container}>
+                            <button className={styles.wallet__button}>Connect Wallet</button>
+                        </div>
+                    </div>
+                    <span className={styles.line1}></span>
+                    <span className={styles.line2}></span>
+                    <span className={styles.line3}></span>
+                </div>
+            </div>
         </nav>
     );
 }
