f88240bf597df908cc02cddc530fde60a7fb7dfc
tochibedford <musicofbyte@gmail.com>
2022-11-11 23:16:50 +0100

    feat find inspiration section title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,8 @@ const Home = () => {
                 </div>
             </span>
             <main className={styles.main__content}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam eaque quo libero placeat repellat, recusandae totam cum dicta dolorum consectetur, repudiandae earum perspiciatis, magnam eum facere nisi inventore nesciunt numquam?
+                <h2 className={styles.inspiration__title}>Inspiration for your next adventure</h2>
+                
             </main>
         </>
     );
