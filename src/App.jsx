0a1cc1588bdc63d2090c8d9f511710105d4646f2
tochibedford <musicofbyte@gmail.com>
2022-11-11 16:24:26 +0100

    feat: implemented left and right hero sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
 import styles from './App.module.scss'
 import Navbar from './components/Navbar'
+import img1 from './assets/img1.png'
+import img2 from './assets/img2.png'
+import img3 from './assets/img3.png'
+import img4 from './assets/img4.png'
 
 function App() {
 
   return (
-    <div className="App">
-      <Navbar />
+    <div className={styles.app}>
+      <header>
+        <Navbar />
+        <div className={styles.hero__container}>
+          <div className={styles.hero__left}>
+            <div className={styles.hero__title}>
+              Rent a <span className={styles.special__text}>Place</span> away from <span className={styles.special__text}>Home</span> in the <span className={styles.special__text}>Metaverse</span>
+            </div>
+            <p className={styles.hero__paragraph}>we provide you access to luxury and affordable houses in the metaverse, get a chance to turn your imagination to reality at your comfort zone</p>
+            <span className={styles.search__container}></span>
+          </div>
+          <div className={styles.hero__right}>
+            <div className={styles.hero__masonry}>
+              <div className={`${styles.masonry__image__container} ${styles.img1__container}`}>
+                <img src={img1} alt="hero image" />
+              </div>
+              <div className={`${styles.masonry__image__container} ${styles.img2__container}`}>
+                <img src={img2} alt="hero image" />
+              </div>
+              <div className={`${styles.masonry__image__container} ${styles.img3__container}`}>
+                <img src={img3} alt="hero image" />
+              </div>
+              <div className={`${styles.masonry__image__container} ${styles.img4__container}`}>
+                <img src={img4} alt="hero image" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
       <main className={styles.main__content}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam eaque quo libero placeat repellat, recusandae totam cum dicta dolorum consectetur, repudiandae earum perspiciatis, magnam eum facere nisi inventore nesciunt numquam?
       </main>
