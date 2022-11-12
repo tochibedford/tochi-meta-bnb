589dbff9b27361209884f334c2038939c7d5a55e
tochibedford <musicofbyte@gmail.com>
2022-11-12 19:39:27 +0100

    feat: implemented wireframe for footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {
   Route,
 } from "react-router-dom";
 import Home from './pages/Home';
+import Footer from './components/Footer';
 
 function App() {
 
@@ -17,7 +18,7 @@ function App() {
       <Routes>
         <Route path="/" element={ <Home /> } />
       </Routes>
-      
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from '../styles/Footer.module.scss'
+
+const Footer = () => {
+    return ( 
+        <footer className={styles.footer}>
+            <div className={styles.footer__left}>1</div>
+            <div className={styles.footer__right}>1</div>
+        </footer>
+    );
+}
+ 
+export default Footer;
