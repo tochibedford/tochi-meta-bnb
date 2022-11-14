7db8adceefed63360c92e00e0311a17aad39f3d4
tochibedford <musicofbyte@gmail.com>
2022-11-14 10:12:36 +0100

    refactor: added prefixes to key attributes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     const cards = []
     for(let i = 0; i<8; i++){
         cards.push(
-            <Card key={i}/>
+            <Card key={"card__" + i}/>
         )
     }
     return (
